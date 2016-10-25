592540f58d5ebd0ad759cfdf54aacd6b0d7203f1
Charles Peters <chpeters13@gmail.com>
2016-10-25 11:58:09 -0400

    added routing and re-organized the folder structures

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
 import './index.css';
+import { Router, Route, browserHistory } from 'react-router';
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render((
+        <Router history={browserHistory}>
+            <Route path="/" component={App} />
+        </Router>
+    ),
   document.getElementById('root')
 );
