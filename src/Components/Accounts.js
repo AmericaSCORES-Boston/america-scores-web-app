6fdccfd8aecc8110c771652c370afd4c95aa2b99
tiroffp@gmail.com <tiroff.p@husky.neu.edu>
2016-11-08 21:43:15 -0500

    created basic Mana

---
+++ src/Components/Accounts.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class AccountsManager extends Component {
+  render() {
+    return (
+      <div className="AccountsManager">
+        <div className="Account-header">
+        <h1>Accounts</h1>
+        </div>
+        <AccountsTable  accounts={this.props.accounts} />
+      </div>
+      );
+  }
+}
+
+class AccountsTable extends Component {
+  render() {
+    var rows = [];
+    this.props.accounts.forEach(function(account) {
+      rows.push(<AccountRow account={account} key={account.name} />);
+    });
+    return (
+      <table>
+      <thead>
+      <tr>
+      <th>Name</th>
+      <th>Account Type</th>
+      <th>Program Permissions</th>
+      </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+      </table>
+      );
+  }
+}
+
+class AccountRow extends Component {
+   render() {
+    return (
+      <tr className="coach-row">
+        <td>{this.props.account.name}</td>
+        <td>{this.props.account.type}</td>
+        <td>{this.props.account.permissions}</td>
+      </tr>
+    );
+  }
+}
+
+export  {
+  AccountsManager,
+  AccountsTable
+};
