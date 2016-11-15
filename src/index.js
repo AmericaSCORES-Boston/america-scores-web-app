ea2257b711dd0eee15051690a2e45e0bc4e50787
Charles Peters <chpeters13@gmail.com>
2016-11-15 11:05:03 -0500

    Added first pass for the homepage design

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
+import ManageAccountsManager from './Components/ManageAccounts';
 import './index.css';
 import { Router, Route, browserHistory } from 'react-router';
 
 ReactDOM.render((
         <Router history={browserHistory}>
             <Route path="/" component={App} />
+            <Route path="/manageAccounts" component={ManageAccountsManager} />
         </Router>
     ),
   document.getElementById('root')
