c6094e5c0e2f8e36147874ceace5fe128258e18b
Peyton Tiroff <tiroffp@ccs.neu.edu>
2016-11-17 18:21:29 -0500

    Revert "AS-44: Basic Student Table"

--- scripts/stop-server.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ -e /amscores/web-app ]; then
-  cd /amscores/web-app
-  rm -rf *
-fi
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,6 @@
-// this is just a test
-
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
-
 
 class App extends Component {
   render() {
@@ -12,7 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
-        </div> 
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
