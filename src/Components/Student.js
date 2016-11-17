e30400e5561f40646cf51de82e7342c697f9ccf5
49527144 <pang.b@husky.neu.edu>
2016-11-17 15:42:38 -0500

    Complete student component and test

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -1,14 +1,13 @@
 class Students extends React.Component {
   render() {
-    var name = this.props.product.stocked ?
-      this.props.product.name :
-      <span style={{color: 'blue'}}>
-        {this.props.product.name}
-      </span>;
     return (
       <tr>
-        <td>{name}</td>
-        <td>{this.props.product.locations}</td>
+        <td style={{color: 'blue'}}>
+          {this.props.product.name}
+        </td>
+        <td style={{color: 'green'}}>
+          {this.props.product.locations}
+        </td>
       </tr>
     );
   }
@@ -17,7 +16,6 @@ class Students extends React.Component {
 class StudentsTable extends React.Component {
   render() {
     var rows = [];
-    var lastCategory = null;
     this.props.products.forEach(function(product) {
       rows.push(<Students product={product} key={product.name} />);
     });
@@ -42,12 +40,33 @@ var testdata = [
   {name: 'D', dob: '11/11/1111', locations: 'l1, l2, l3'},
   {name: 'E', dob: '11/11/1111', locations: 'l1, l2, l3'}
 ];
- 
+
+//var reactLogo = require('../Assets/User.png');
+
+class page extends React.Component {
+  render() {
+    return (
+        <div classname = "studentpage">
+        <div classname = "header">
+          <h1> STUDENT </h1>
+        </div>
+        <img src="http://placehold.it/400x20undefined1" data-reactid=".0.0"/>
+        <StudentsTable products={testdata} />
+        </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <div>
   <h1> STUDENT </h1>
-  <img src={'http://placehold.it/400x20&text=slide1'}/>
+  <img src="http://placehold.it/400x20undefined1" data-reactid=".0.0"/>
   <StudentsTable products={testdata} />
   </div>,
   document.getElementById('container')
 );
+
+// export  {
+//   StudentsTable,
+//   page
+// };
--- src/Components/__tests__/Student.test.js
+++ src/Components/__tests__/Student.test.js
@@ -1,42 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import {StudentsManager,StudentsTable} from '../Student.js';
+import {page,StudentsTable} from '../Student.js';
 
-var testdata = [
-  {name: 'A', dob: '11/11/1111', locations: 'l1, l2, l3'},
-  {name: 'B', dob: '11/11/1111', locations: 'l1, l2, l3'},
-  {name: 'C', dob: '11/11/1111', locations: 'l1, l2, l3'},
-  {name: 'D', dob: '11/11/1111', locations: 'l1, l2, l3'},
-  {name: 'E', dob: '11/11/1111', locations: 'l1, l2, l3'}
-];
 
 it('renders fully without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<StudentsManager accounts={[]} />, div);
+  ReactDOM.render(<page accounts={[]} />, div);
 });
 
-describe('the StudentsManager', () => {
+describe('student page', () => {
     it('renders correctly', () => {
         const render = TestUtils.createRenderer()
-        render.render(<StudentsManager />)
+        render.render(<page />)
         var result = render.getRenderOutput();
         expect(result.type).toBe('div');
-        expect(result.props.className).toBe('StudentsManager')
-        expect(result.props.children[0].props.className).toBe('Students-header')
-        expect(result.props.children[0].props.children.type).toBe('h1')
-    });
-});
-
-describe('the list of Students', () => {
-        var studentsManager = <StudentsTable accounts={testdata} />
-        studentsManager = TestUtils.renderIntoDocument(studentsManager);
-        var rows = TestUtils.scryRenderedDOMComponentsWithClass(studentsManager, 'Students-row')
-        var columns = TestUtils.scryRenderedDOMComponentsWithTag(studentsManager, 'th')
-    it('has the correct number of rows', () => {
-        expect(rows.length).toBe(testdata.length)
-    });
-    it('has the correct number of columns', () =>{
-        expect(columns.length).toBe(2)
+        expect(result.props.className).toBe('studentpage');
+        expect(result.props.children[0].props.className).toBe('header');
+        expect(result.props.children[0].props.children.type).toBe('h1');
+        expect(result.props.children[1].props.type).toBe('img');
     });
 });
