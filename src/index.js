6f5c4d92ced0b6ec944bf5a26e5e4ef542b75066
Peyton Tiroff <tiroffp@gmail.com>
2016-11-17 17:49:15 -0500

    added routing to students component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-import ManageAccountsManager from './Components/ManageAccounts';
+import page from './Components/Student';
 import './index.css';
 import { Router, Route, browserHistory } from 'react-router';
 
 ReactDOM.render((
         <Router history={browserHistory}>
             <Route path="/" component={App} />
-            <Route path="/manageAccounts" component={ManageAccountsManager} />
+            <Route path="/Students" component={page} />
         </Router>
     ),
   document.getElementById('root')
