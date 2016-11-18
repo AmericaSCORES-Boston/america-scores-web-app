21b42143dd8584218519bea5fb66aa0cecc4419d
alshammarif <f.al.shammari995@gmail.com>
2016-11-18 11:22:52 -0500

    Update MyAccount.test.js

--- src/Components/__tests__/MyAccount.test.js
+++ src/Components/__tests__/MyAccount.test.js
@@ -5,9 +5,9 @@ import {MyAccountConstants, MyAccountCompiler, MyAccountsData} from '../MyAccoun
 
 //Not finalized since I am unsure how the exact syntax is. But It gives the general idea of what I want.
 
-var testA = {name: "Alice", email: "alice@email.com", phone: 503-667-2134, password: "******"};
+var data = {name: "Alice", email: "alice@email.com", phone: 5036672134, password: "******"};
 
-var testB = {name: "Katniss", email: "katniss@email.com", phone: "877-332-4543", password: "******"};
+var cons = {title: "Name:", etitle: "Email:", ptitle: "Phone:", pwtitle: "Password:"};
 
 //renders the most basic page without any data. 
 it('renders fully without crashing', () => {
@@ -15,27 +15,11 @@ it('renders fully without crashing', () => {
   ReactDOM.render(<MyAccountCompiler data={[]} />, div);
 });
 
-//This tests the MyAccountConstants class
-describe('the MyAccountConstents', () =>
-	it('renders correctly', () =>
-		const render = ReactTestUtils.createRenderer();
-		render.render(<MyAccountConstents />)
-		var results = render.getRenderOutput();
-		expect(results.type).toBe('div'));
-		expect(results.props.className).toBe('constents');
-		expect(results.props.children[0].type).toBe('h1');
-	);
-);
-
-//This tests the MyAccountsData class
-describe('the MyAccountsData', () =>
-	var myaccdataA = <MyAccountData data={testA} />
-	myaccdataA = ReactTestsUtils.renderIntoDocument(myaccdataA);
-	it('has correct number of items', () =>
-		expect(testA.length).toBe(4);
-	);
-	it('renders the correct information', () =>
-		expect(myaccdataA.props.children[1]).toBe("Alice");
-		expect(myaccdataA.props.children[2]).toBe("alice@email.com");
-	);
-);
+describe('the MyAccountCompiler', () => {
+    it('renders correctly', () => {
+        const render = TestUtils.createRenderer()
+        render.render(<MyAccountCompiler data={[]}/>)
+        var result = render.getRenderOutput();
+        expect(result.type).toBe('div');
+    });
+});
