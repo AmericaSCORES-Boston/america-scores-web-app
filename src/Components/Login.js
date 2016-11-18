503e3632bf221f67f95acfbaf498cef2e12308f3
Amal Nazeem <amalnazeem@gmail.com>
2016-11-18 02:34:08 -0500

    made tweaks to index's routing and Download Page and Login Page. Made some minor syntax tweaks in Student.js to avoid warnings thrown by react.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,7 +6,7 @@ class Login extends Component {
   render() {
     return (
       <div className="form">
-          <form action="/manageAccounts">
+          <form action="/MyAccount">
             <input type="text" name="username" placeholder="username"/>
             <input type="password" name="password" placeholder="password"/>
             <button type="submit">Login</button>
