03b301e42b590fab81dd9550db81f8b5f46d7dd3
alshammarif <f.al.shammari995@gmail.com>
2016-11-18 23:55:59 -0500

    Took Out the Contents Function. It was redundant.

--- src/Components/MyAccount.js
+++ src/Components/MyAccount.js
@@ -3,35 +3,16 @@ import ReactDOM from 'react-dom';
 
 class MyAccountCompiler extends React.Component {
 	
-  function RenConstants(props) {
-    const content = props.constants.map((constant) =>
-      <div>
-       <h3>{constant.ntitle}</h3>
-       <br />
-       <h3>{constant.etitle}</h3>
-       <br />                                  
-        <h3>{constant.ptitle}</h3>  
-       <br />                                  
-        <h3>{constant.pwtitle}</h3>         
-      </div>
-    );
-    return (
-      <div>
-       {content}
-      </div>
-    );
-  } 
-
   function RenData(props) {
     const content = props.data.map((adata) =>
       <div>
-       <h3>{adata.name}</h3>
+       <p><strong>Name:</strong> {adata.name}</p>
         <br />
-        <h3>{adata.email}</h3>
+        <p><strong>Email:</strong> {adata.email}</p>
         <br />                                 
-        <h3>{adata.phone}</h3>  
+        <p><strong>Phone:</strong>{adata.phone}</p>  
         <br />                                  
-        <h3>{adata.password}</h3>         
+        <p><strong>Password:</strong>{adata.password}</p>         
     </div>
     );
   return (
@@ -43,11 +24,9 @@ class MyAccountCompiler extends React.Component {
 
 render () {
 		const data=[];
-		const constants = [{ntitle: "Name:", etitle: "Email:", ptitle: "Phone:", pwtitle: "Password:"}];
 		<div className="all">
 		<center><h1>My Account</h1>
-		<RenConstants constants={constants} />
-		<RenData data={data} />
+    <RenData data={data} />
 		</center>
 		</div>
 	}
