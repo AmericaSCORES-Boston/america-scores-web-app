61224da44fb06656561c655ada4e804bbdea90bf
alshammarif <f.al.shammari995@gmail.com>
2016-11-28 16:54:54 -0500

    Update MyAccount.test.js

--- src/Components/__tests__/MyAccount.test.js
+++ src/Components/__tests__/MyAccount.test.js
@@ -6,15 +6,15 @@ import {MyAccountCompiler} from '../MyAccount';
 //renders the most basic page without any data. 
 it('renders fully without crashing', () => {
   const div = document.createElement('div');
-//   ReactDOM.render(<MyAccountCompiler data={[]} />, div);
+  ReactDOM.render(<MyAccountCompiler data={[]} />, div);
 });
 
 describe('the MyAccountCompiler', () => {
     it('renders correctly', () => {
         const render = TestUtils.createRenderer()
         render.render(<MyAccountCompiler />)
-//         var result = render.getRenderOutput();
-//         expect(result.type).toBe('div');
+        var result = render.getRenderOutput();
+        expect(result.type).toBe('div');
     });
 });
 
