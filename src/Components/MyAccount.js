8f2a23c2ead33952bc7cd5eda232eac62f3fee58
alshammarif <f.al.shammari995@gmail.com>
2016-11-27 23:19:47 -0500

    Fixed My Accounts Page
    This is the correct syntax.

--- src/Components/MyAccount.js
+++ src/Components/MyAccount.js
@@ -5,41 +5,36 @@ var icon = require('../Assets/User-icon.png');
 
 class MyAccountCompiler extends React.Component {
 	
-  function RenData(props) {
-    const content = props.data.map((adata) =>
-      <div>
-       <p><strong>Name:</strong> {adata.name}</p>
-        <br />
-        <p><strong>Email:</strong> {adata.email}</p>
-        <br />                                 
-        <p><strong>Phone:</strong>{adata.phone}</p>  
-        <br />                                  
-        <p><strong>Password:</strong>{adata.password}</p>         
-    </div>
-    );
-  return (
-    <div>
-      {content}
-    </div>
-  );
-}
+render() {
+	function renData(props) {
+    		const content = props.data.map((adata) =>
+      			<div>
+       				<p><strong>Name:</strong> {adata.name}</p>
+        			<br />
+        			<p><strong>Email:</strong> {adata.email}</p>
+        			<br />                                 
+        			<p><strong>Phone:</strong>{adata.phone}</p>  
+        			<br />                                  
+        			<p><strong>Password:</strong>{adata.password}</p>         
+    			</div>
+    	);
+    	
+  		return (
+    		<div>
+      		{content}
+    		</div>
+  		);
+	};
+	
+	var data = {name: "Alice", email: "alice@email.com", phone: 5036672134, password: "******"};
 
-render () {
-		const data=[];
+	return(
 		<div className="all">
-		<center><h1>My Account</h1>
-		<img src={icon} alt="UserIcon" data-reactid=".0.0"/>
-    		<RenData data={data} />
-		</center>
+			<center><h1>My Account</h1>
+			<img src={icon} alt="UserIcon" data-reactid=".0.0"/>      
+    		<renData data={data} />
+			</center>
 		</div>
-	}
-}
-
-RenderDOM.render(
-  <MyAccountCompiler data={[]} />
-  document.getElementById('root')
-  );
-
-export {
-	MyAccountCompiler
-}
+		);
+	};
+};
