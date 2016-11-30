7bbceee8e306deea58a8b1179294ef570508c512
alshammarif <f.al.shammari995@gmail.com>
2016-11-29 22:12:22 -0500

    Update MyAccount.test.js

--- src/Components/__tests__/MyAccount.test.js
+++ src/Components/__tests__/MyAccount.test.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import {MyAccountManager} from '../MyAccounts.js';
+import {MyAccountCompiler} from '../MyAccount';
 
-//Not finalized since I am unsure how the exact syntax is. But It gives the general idea of what I want.
-
-const data = {name: "Alice", email: "alice@email.com", phone: 5036672134, password: "******"};
 
 //renders the most basic page without any data. 
 it('renders fully without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<MyAccountManager data={[]} />, div);
+  ReactDOM.render(<MyAccountCompiler data={[]} />, div);
 });
 
-describe('My Account Page', () => {
+describe('the MyAccountCompiler', () => {
     it('renders correctly', () => {
         const render = TestUtils.createRenderer()
-        render.render(<MyAccountManager />)
+        render.render(<MyAccountCompiler />)
         var result = render.getRenderOutput();
         expect(result.type).toBe('div');
-        expect(result.props.children[0].type).toBe('h1');
     });
 });
