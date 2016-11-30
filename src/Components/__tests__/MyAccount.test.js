b4b0dbf40107401b92dca201197a89f218e9f35c
alshammarif <f.al.shammari995@gmail.com>
2016-11-29 22:31:29 -0500

    Update MyAccount.test.js

--- src/Components/__tests__/MyAccount.test.js
+++ src/Components/__tests__/MyAccount.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import {MyAccountCompiler} from '../MyAccount';
+import MyAccountCompiler from '../MyAccount';
 
 
-//renders the most basic page without any data. 
 it('renders fully without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MyAccountCompiler data={[]} />, div);
 });
 
-describe('the MyAccountCompiler', () => {
+describe('student page', () => {
     it('renders correctly', () => {
         const render = TestUtils.createRenderer()
         render.render(<MyAccountCompiler />)
