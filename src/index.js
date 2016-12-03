a68bb5584b0e0bf26e46dd1172897359bc7dd44b
Charlie Peters <Charles@cmp.hsd1.ma.comcast.net>
2016-12-03 16:31:39 -0500

    Added Sites

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import StudentManager from './Components/Student';
 import './index.css';
 import { Router, Route, browserHistory } from 'react-router';
 import ManageAccountsManager from './Components/ManageAccounts';
-import RecordResponse from './Components/RecordResponse'
+import RecordResponse from './Components/RecordResponse';
+import Sites from './Components/Sites';
 
 
 ReactDOM.render((
         <Router history={browserHistory}>
             <Route path="/" component={Login} />
+            <Route path="/Sites" component={Sites} />
             <Route path="/CsvPage" component={CSVPage} />
             <Route path="/Students" component={StudentManager} />
             <Route path="/ManageAccounts" component={ManageAccountsManager} />
