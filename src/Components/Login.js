b001ace407bcd2b65a1bfbf84f933da3fc701130
tiroffp@gmail.com <tiroff.p@husky.neu.edu>
2016-12-03 19:57:52 -0500

    fixed dependency issues

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,5 @@
 import '../Login.css';
 import React, { PropTypes as T } from 'react'
-import {ButtonToolbar, Button, Jumbotron} from 'react-bootstrap'
 import AuthService from '../utils/AuthService'
 import asLogo from '../Assets/aslogo.png'
 
@@ -13,15 +12,11 @@ export class Login extends React.Component {
     const { auth } = this.props
     return (
       <div className="root">
-        <Jumbotron>
         <h2 className="root">
           <img src={asLogo} width="150px" height="168px" alt="AS Logo Icon" />
         </h2>
         <h2>Login</h2>
-        <ButtonToolbar className="toolbar">
-          <Button bsStyle="primary" onClick={auth.login.bind(this)}>Login</Button>
-        </ButtonToolbar>
-        </Jumbotron>
+          <button bsStyle="primary" onClick={auth.login.bind(this)}>Login</button>
       </div>
     )
   }
