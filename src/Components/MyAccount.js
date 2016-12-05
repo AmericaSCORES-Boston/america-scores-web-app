59d2b345019917c0878b9bf5f4486bb687ac1ac0
alshammarif <f.al.shammari995@gmail.com>
2016-12-04 19:38:04 -0500

    Update MyAccount.js

--- src/Components/MyAccount.js
+++ src/Components/MyAccount.js
@@ -1,52 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import '../MyAccount.css';
+import icon from '../Assets/User-icon.png';
 //import Api from '../api';
 
-//The User Account Icon.
-var icon = require('../Assets/User-icon.png');
+class MyAccountCompiler extends Component {	
+render() {	
+  function renData(constant, rdata) {
+      console.log('calling rendata'); 
+     return (<div><p><strong>{constant}</strong>{rdata}</p></div>)
 
-//This renders out the data of the most updated version of the User's own Account information 
-//for them to view 
-class MyAccountCompiler extends React.Component {	
+  }; 
 
-	render() {	
-		function renData(props) {
-			const content = props.data.map((adata) =>
-						 <div className="userData">
-						       <p><strong>Name:</strong> {adata.name}</p>
-						       <br />
-						       <p><strong>Email:</strong> {adata.email}</p>
-						       <br />
-						       <p><strong>Phone:</strong>{adata.phone}</p>
-						       <br />
-						       <p><strong>Password:</strong>{adata.password}</p>
-						</div>
-					);
-    	
-  			return (
-    				<div className="displayData">
-      					{content}
-    				</div>
-  			);	
-		};
-		
-		var data = {name: "Alice", email: "alice@email.com", phone: 5036672134, password: "******"};	
-		return(		
-				<div className="all">			
-					<div className="Header">				
-						<div className="Title">				
-							<h1>My Account</h1>				
-						</div>				
-						<div className="PageIcon">				
-							<img src={icon} alt="UserIcon" data-reactid=".0.0"/> 				
-						</div>			
-					</div>			
-					<div className="dataMethod">				
-						<renData data={data} />			
-					</div>		
-				</div>		
-		);	
-	};
+const data = { name: "Alice", email: "alice@email.com", phone: 5036672134, password: "******"};
+  return(		
+    <div className="all">			
+      <div className="Header">				
+        <div className="Title">				
+          <h1>My Account</h1>				
+        </div>				
+        <div className="PageIcon">				
+          <img src={icon} alt="UserIcon" data-reactid=".0.0"/> 				
+        </div>			
+      </div>			
+      <div className="dataMethod">
+        { renData( "Name:", data.name) }
+        { renData("Email:", data.email) }
+        { renData( "Phone:", data.phone) }
+        { renData( "Password:", data.password) }
+      </div>		
+    </div>		
+  );	
+
+}
 };
+
+ 
 
 export default MyAccountCompiler;
