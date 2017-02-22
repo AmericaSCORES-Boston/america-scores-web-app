d2a90f720d0813a90cbd0928e0535a8dd6629ae6
Kaila <kcorring@ccs.neu.edu>
2017-02-22 15:28:27 -0500

    npm command shouldn't be a directory

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,9 +4,9 @@ cd /amscores/web-app
 if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]
 then
     export NODE_ENV=production
-    /usr/bin/npm/ build
+    /usr/bin/npm build
 else
     export NODE_ENV=development
-    /usr/bin/npm/ start
+    /usr/bin/npm start
 fi
 
--- scripts/stop-server.sh
+++ scripts/stop-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ -e /amscores/web-app ]; then
   cd /amscores/web-app
-  /usr/bin/npm/ stop
+  /usr/bin/npm stop
 fi
