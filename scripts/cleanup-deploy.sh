5464399bebda657315a0f51def6785ef109c6926
Kaila <kcorring@ccs.neu.edu>
2017-02-22 15:01:37 -0500

    First pass at spinning up web server on AWS
    Adds lifecycle event scripts to mimic backend, temporarily links to this
    testing branch, and initial attempt at stopping the server using process
    title ... which doesn't work on my machine, so we'll see how it does on
    EC2.
    
    [AS-225]

--- scripts/cleanup-deploy.sh
+++ scripts/cleanup-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ -e /amscores/web-app ]; then
   cd /amscores/web-app
-  /usr/bin/npm/ stop
+  rm -rf *
 fi
---
+++ scripts/start-server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd /amscores/web-app
+if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]
+then
+    export NODE_ENV=production
+    /usr/bin/npm/ build
+else
+    export NODE_ENV=development
+    /usr/bin/npm/ start
+fi
+
