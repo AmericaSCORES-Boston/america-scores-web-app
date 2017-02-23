d1ce9b2dd88cc031801553bf27b8f44ac44a1d00
Kaila <kcorring@ccs.neu.edu>
2017-02-23 08:27:11 -0500

    Build creates static, don't use pm2 to serve ?

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,7 +4,6 @@ cd /amscores/web-app
 if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]
 then
     export NODE_ENV=production
-    /usr/local/bin/pm2 start scripts/build.js -n amscores_webapp
 else
     export NODE_ENV=development
     /usr/local/bin/pm2 start scripts/start.js -n amscores_webapp
