6af729b3fcc55b17da683bd4432bf82d02df127d
Kaila <kcorring@ccs.neu.edu>
2017-03-03 13:25:25 -0500

    Get this branch setup with AWS
    This branch will trigger AWS builds and deployment
    
    [AS-261][AS-262]

---
+++ scripts/setup-server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /amscores/web-app
+
+npm install -g pushstate-server
---
+++ scripts/start-server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd /amscores/web-app
+if [ "$DEPLOYMENT_GROUP_NAME" == "Production" ]
+then
+    export NODE_ENV=production
+else
+    export NODE_ENV=development
+fi
+
+pushstate-server build > /dev/null 2> /dev/null < /dev/null &
+
