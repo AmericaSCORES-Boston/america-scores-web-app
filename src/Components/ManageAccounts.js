1b141ee72993cb85316e7edd678493ff31b39c8b
Matt Harty <harty.m@husky.neu.edu>
2017-04-05 14:13:56 -0400

    fixed coaches page to use an api stub

--- src/Components/ManageAccounts.js
+++ src/Components/ManageAccounts.js
@@ -1,32 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
 import icon from '../Assets/Accounts-icon.png';
+import Api from '../api';
 
 /*
 Gets all the Coaches and their permissions
 Currently the API endpoint for coaches is unimplented so it is running on test data
 */
-class ManageAccountsManager extends Component {
-  render() {
-    function onRowSelect(row, isSelected){
-      console.log(row);
-      console.log("selected: " + isSelected)
-    }
-    var testdata = [
-    {name: 'Guy 1', type: 'Coach', permissions: 'Program 1, Program 2'},
-    {name: 'Guy 2', type: 'Volunteer', permissions: 'Program 3'},
-    {name: 'Guy 3', type: 'Coach', permissions: 'Program 4'},
-    {name: 'Girl 1', type: 'Volunteer', permissions: 'Program 2'},
-    {name: 'Girl 2', type: 'Coach', permissions: 'Program 4'},
-    {name: 'Girl 3', type: 'Coach', permissions: 'Program 5'},
-    ];
+var ManageAccountsManager = React.createClass ({
+  getInitialState: function() {
+      return {
+        coachData: []
+      }
+    },
+
+  componentDidMount: function() {
+      let _this = this;
+      //TODO stub data
+      var testData = [
+                      {name: 'Guy 1', type: 'Coach', email: 'test@email.com'},
+                      {name: 'Guy 2', type: 'Volunteer', email: 'test@email.com'},
+                      {name: 'Guy 3', type: 'Coach', email: 'test@email.com'},
+                      {name: 'Girl 1', type: 'Volunteer', email: 'test@email.com'},
+                      {name: 'Girl 2', type: 'Coach', email: 'test@email.com'},
+                      {name: 'Girl 3', type: 'Coach', email: 'test@email.com'},
+                     ];
+      let data = [];
+      //let queryParamProgramId = this.props.location.query.program;
+
+      //add all coaches
+      Api.fetchByAccountType('Coach').then(json => {
+        for (let i = 0; i < json.length; i++) {
+          data.push({name: (json[i].first_name + " " + json[i].last_name), type: json[i].acct_type, email: json[i].email});
+        }
+        _this.setState({
+            //TODO giving the stub data to the state
+          coachData : testData
+        })
+      });
+
+      //add all volunteers
+      Api.fetchByAccountType('Volunteer').then(json => {
+        for (let i = 0; i < json.length; i++) {
+          data.push({name: (json[i].first_name + " " + json[i].last_name), type: json[i].acct_type, email: json[i].email});
+        }
+        _this.setState({
+            //TODO giving the stub data to the state
+          coachData : testData
+        })
+      });
+    },
+
+    render: function() {
+      let _this=this;
+      function onRowSelect(row, isSelected){
+        console.log(row);
+        console.log("selected: " + isSelected)
+      }
+
     var selectRowProp = {
       mode: "radio",
       clickToSelect: true,
       bgColor: "rgb(238, 193, 213)",
       onSelect: onRowSelect,
       hideSelectColumn: true
-    };
+    }
     return (
       <div className="container-fluid">
       <div className="row">
@@ -34,15 +72,15 @@ class ManageAccountsManager extends Component {
       <div className="col-xs-6 text-center">
       <img src={icon} className="img-responsive center-block" alt="logo" />
       <h1 className="Account-header"> Coaches </h1>
-      <BootstrapTable data={testdata} triped={true} hover={true} condensed={true} selectRow={selectRowProp}>
+      <BootstrapTable data={this.state.coachData} triped={true} hover={true} condensed={true} selectRow={selectRowProp}>
       <TableHeaderColumn isKey={true} dataField="name">Name</TableHeaderColumn>
       <TableHeaderColumn dataField="type">Account Type</TableHeaderColumn>
-      <TableHeaderColumn dataField="permissions">Program Permissions</TableHeaderColumn>
+      <TableHeaderColumn dataField="email">Program Permissions</TableHeaderColumn>
       </BootstrapTable>
       </div>
       </div>
       </div>
       );
   }
-}
+})
 export  default  ManageAccountsManager;
