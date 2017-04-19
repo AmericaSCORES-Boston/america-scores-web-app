038d03a6ad2ed0326be2ed498386e3bf21fb8d75
Matt Harty <harty.m@husky.neu.edu>
2017-04-19 15:45:28 -0400

    forgot one change in last push

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -25,7 +25,7 @@ export default class AuthService extends EventEmitter {
   }
 
   _doAuthentication(authResult){
-    console.log('it authed:' + authResult)
+    console.log('it authed:')
     // Saves the user token
     this.setToken(authResult.idToken)
     // navigate to the home route
